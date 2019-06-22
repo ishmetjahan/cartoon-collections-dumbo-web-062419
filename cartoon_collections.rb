f7e53721f5f46395f3efb59959ca95ea91782519
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 12:01:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (arr) 
+  i=0 
+  while i<arr.length 
+  puts "#{i+1}. #{arr[i]}"
+  i += 1
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (arr)
+  newArr= []
+arr.
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
 end
 
 def find_the_cheese# code an argument here
